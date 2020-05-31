83649bb3da2953cbef48813ea9e75f6651f6bb7a
rgberou <brotark@gmail.com>
2020-05-31 09:06:48 +0800

    Updated css

--- assignment-app/src/App.js
+++ assignment-app/src/App.js
@@ -6,7 +6,7 @@ import UserOutput from './UserOutput/UserOutput';
 class App extends Component {
 
   state={
-    userName: "Argie"
+    userName: "The instructions are: \r\n    Create TWO new components: UserInput and UserOutput\r\n    UserInput should hold an input element, UserOutput two paragraphs\r\n    Output multiple UserOutput components in the App component (any paragraph texts of your choice)\r\n    Pass a username (of your choice) to UserOutput via props and display it there\r\n    Add state to the App component (=> the username) and pass the username to the UserOutput component\r\n    Add a method to manipulate the state (=> an event-handler method)\r\n    Pass the event-handler method reference to the UserInput component and bind it to the input-change event\r\n    Ensure that the new input entered by the user overwrites the old username passed to UserOutput\r\n    Add two-way-binding to your input (in UserInput) to also display the starting username\r\n    Add styling of your choice to your components\/ elements in the components - both with inline styles and stylesheets"
   }
 
   userNameChangedHandler = (event) => {
--- assignment-app/src/UserInput/UserInput.js
+++ assignment-app/src/UserInput/UserInput.js
@@ -3,7 +3,7 @@ import React from 'react';
 const userInput = (props) => {
     return (
         <div>
-            <input type="text"  onChange={props.changed} value={props.userName}/>
+            <input type="text"  onChange={props.changed} />
         </div>
     );
 
